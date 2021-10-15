bf05bdda16679875a2bebfd29ed8edfe91b39882
Jonathan <82249401+jonathho@users.noreply.github.com>
2021-10-15 14:53:03 -0700

    generalizing for other use cases

--- JonathanH/Genus_Abundance_Plot.R
+++ JonathanH/Genus_Abundance_Plot.R
@@ -1,56 +1,58 @@
 # This script plots a stacked bar graph for samples containing percentage data.
 # The input is genus clade percent data from Pavian (kraken reports)
 # that have Eukaryota filtered out.
+
 # To download the data in a script-ready form, go to the Comparison tab in
 # Pavian, filter out Eukaryota, ensure clade is selected and select percent.
+
+# TODO:'s show recommended fields that should be changed for each analysis
 
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+# TODO: change file path
+datapath <- '2020_ctx_kraken2/ctx_kraken_genus_percent.tsv'
+
 # read data
-ctx_data <- read.delim(file = 'ctx_kraken_genus_percent_data.tsv',
+data <- read.delim(file = datapath,
                        header = TRUE,
                        sep = '\t')
 
 # clean data
-clean_ctx_data <- select(ctx_data, -taxRank, -taxID, -Max, -lineage) %>%
+clean_data <- select(data, -taxRank, -taxID, -Max, -lineage) %>%
   pivot_longer(!name, names_to = "sample", values_to = "percent") %>%
   pivot_wider(names_from = "name", values_from = "percent")
 
 # scale data and convert to data frame
-scaled_ctx_data <- apply(clean_ctx_data[, -1],
+scaled_data <- apply(clean_data[, -1],
                          MARGIN = 1,
                          FUN = function(x) x / sum(x)) %>%
   t() %>%
   as.data.frame()
 
 # select for core taxa
-core_data <- select(scaled_ctx_data,
+core_data <- select(scaled_data,
                     "Gilliamella",
                     "Snodgrassella",
                     "Bifidobacterium",
                     "Lactobacillus",
                     "Frischella")
 
-# add treatment and replicate info
-treatments <- c("Control", "CLO", "THI",
-                "Control", "CLO", "THI",
-                "Control", "CLO", "THI",
-                "Control", "CLO", "THI",
-                "Control", "CLO", "THI")
+# TODO: setup treatment info
+treatments <- rep(c("Control", "CLO", "THI"), 5)
 
-# setup replicate info
+# TODO: setup replicate info
 replicates <- c("Rep 2","Rep 2","Rep 2",
                 "Rep 3","Rep 3","Rep 3",
                 "Rep 4","Rep 4","Rep 4",
                 "Rep 5","Rep 5","Rep 5",
                 "Rep 6","Rep 6","Rep 6")
 
-core_data$replicate <- replicates
 core_data$treatment <- treatments
+core_data$replicate <- replicates
 
-# for ordering on plots
+# TODO: for ordering on plots
 order <- c("Control", "CLO", "THI")
 
 # adjust factor levels for ordering
@@ -59,17 +61,18 @@ core_data$treatment <- factor(core_data$treatment,
 
 # convert data frame into "long" format for stacked bar plot
 long_data <- pivot_longer(core_data,
-                          cols = 1:5,
+                          cols = 1:(ncol(core_data)-2),
                           names_to = "clade",
                           values_to = "percentage")
 
 # plot data
+# TODO: change title label
 abundance_plot <- ggplot(long_data, aes(x = treatment,
                                         y = percentage,
                                         fill = clade)) +
   geom_bar(stat = "identity", colour = "black") +
   facet_grid(~replicate) +
-  labs(title = "Abundance Analysis Using Percent(%) Data",
+  labs(title = "CTX Abundance Using Percent(%) Data",
        x = "Treatment",
        y = "Relative Abundance(%)",
        fill = "Genus") +
@@ -79,7 +82,8 @@ abundance_plot <- ggplot(long_data, aes(x = treatment,
 
 abundance_plot
 
-# save plot as svg
-# svg("Abundance_Plot.svg")
+# un-comment last 3 lines to save plot as svg
+# TODO: change file name
+# svg("CTX_Abundance_Plot.svg")
 # abundance_plot
 # dev.off()
