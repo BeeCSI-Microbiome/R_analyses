a986d56b75e8fc71cfd6e5d683e5faf6ac2769e5
LLansing <35748909+LLansing@users.noreply.github.com>
2021-11-04 14:29:25 -0700

    Update reads_summary.R
    Add percent Apis of classified reads

--- lance/reads_summary.R
+++ lance/reads_summary.R
@@ -40,12 +40,13 @@ ta <- setNames(ta, c('sample','unclassified_reads','classified_reads','Apis_mell
 ta <- mutate(ta, total_reads = unclassified_reads+classified_reads) 
 ta <- mutate(ta, percent_classified = classified_reads/total_reads*100)
 ta <- mutate(ta, percent_unclassified = unclassified_reads/total_reads*100)
-ta <- mutate(ta, percent_Apis_mellifera = Apis_mellifera_reads/total_reads*100)
+ta <- mutate(ta, percent_total_Apis_mellifera = Apis_mellifera_reads/total_reads*100)
+ta <- mutate(ta, percent_classified_Apis_mellifera = Apis_mellifera_reads/classified_reads*100)
 # meta data 
 ta <- mutate(ta, exposure=ifelse(endsWith(sample, "e"), "exposed", "unexposed"))
 ta <- mutate(ta, replicate=str_extract(sample, '\\d\\d'))
 
 # Reorder columns
-ta <- ta[, c(1,9,10,5,2,7,3,6,4,8)]
+ta <- ta[, c(1,10,11,5,2,7,3,6,4,8,9)]
 
 write.table(ta, file='results/read_summary.tsv', quote=F, sep='\t', row.names=F)
