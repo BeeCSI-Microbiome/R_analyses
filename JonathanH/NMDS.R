02ccfa02ceb8a2f289ad74f4a41dcd0c4552ebd8
Jonathan <82249401+jonathho@users.noreply.github.com>
2021-11-05 12:33:01 -0700

    now uses input file containing all taxa

--- JonathanH/NMDS.R
+++ JonathanH/NMDS.R
@@ -1,12 +1,13 @@
-# This script produces an NMDS plot using species percentage data.
-# The input is species percent data from Pavian (kraken reports).
+# This script produces an NMDS plot using percentage data.
+# The input is percent data downloaded from the Comparison tab in Pavian
+# without collapsing taxa.
 
 # To download the data in a script-ready form, go to the Comparison tab in
-# Pavian, filter out Eukaryota, ensure clade is selected and select percent.
+# Pavian, un-collapse the taxa, and download.
 
 # TODO:'s show recommended fields that should be changed for each analysis
 
-# Leave in more info? bees and eukaryotes to capture more info?
+# Script Author(s): Jonathan Ho
 
 library(dplyr)
 library(tidyr)
@@ -14,7 +15,16 @@ library(vegan)
 library(ggplot2)
 
 # TODO: change file path
-datapath <- '2020_ctx_kraken2/ctx_kraken_genus_percent.tsv'
+datapath <- '2020_ctx_kraken2/ctx_kraken_all_percent_uncollapsed.tsv'
+
+# TODO: setup treatment info
+treat_names <- c("Control", "CLO", "THI")
+
+# TODO: setup replicate info
+rep_names <- c("Rep 2", "Rep 3", "Rep 4", "Rep 5", "Rep 6")
+
+# TODO: give a title for the plot
+plot_title <- "CTX Bifidobacterium Percent Abundance"
 
 # read data
 data <- read.delim(file = datapath,
@@ -22,9 +32,11 @@ data <- read.delim(file = datapath,
                        sep = '\t')
 
 # clean data
-clean_data <- select(data, -taxRank, -taxID, -Max, -lineage) %>%
+clean_data <- filter(data, taxRank == "G") %>%
+  select(-taxRank, -taxID, -Max, -lineage) %>%
   pivot_longer(!name, names_to = "sample", values_to = "percent") %>%
-  pivot_wider(names_from = "name", values_from = "percent")
+  pivot_wider(names_from = "name", values_from = "percent")%>%
+  select(-Apis)
 
 # convert taxa part of data frame to matrix for nmds
 taxa_data <- clean_data[,2:ncol(clean_data)]
@@ -34,34 +46,30 @@ taxa_mat <- as.matrix(taxa_data)
 set.seed(1)
 nmds = metaMDS(taxa_mat, distance = "bray")
 
-# TODO: setup treatment info
-treatments <- rep(c("Control", "CLO", "THI"), 5)
-
-# TODO: setup replicate info
-replicates <- c("Rep 2","Rep 2","Rep 2",
-                "Rep 3","Rep 3","Rep 3",
-                "Rep 4","Rep 4","Rep 4",
-                "Rep 5","Rep 5","Rep 5",
-                "Rep 6","Rep 6","Rep 6")
-
 # add back information to matrix
 nmds_data <- as.data.frame(scores(nmds))
 nmds_data$sample <- clean_data$sample
-nmds_data$replicate <- replicates
-nmds_data$treatment <- treatments
 
-# TODO: for ordering treatments on plots
-order <- c("Control", "CLO", "THI")
+num_treats <- length(treat_names)
+num_reps <- length(rep_names)
+
+treatments <- rep(treat_names, num_reps)
+replicates <- c()
+for (r in 1:num_reps) {
+  replicates = c(replicates, rep(rep_names[r], num_treats))
+}
+
+nmds_data$treatment <- treatments
+nmds_data$replicate <- replicates
 
 # adjust factor levels for ordering
 nmds_data$treatment <- factor(nmds_data$treatment,
-                              levels = order)
+                              levels = treat_names)
 
 # plot data
-# TODO: change title label
 nmds_plot <- ggplot(nmds_data, aes(x = NMDS1, y = NMDS2)) + 
   geom_point(aes(shape = treatment, colour = replicate), size = 5) +
-  labs(title = "CTX NMDS Species Percent",
+  labs(title = plot_title,
        x = "NMDS1", 
        y = "NMDS2", 
        shape = "Treatment", 
@@ -70,7 +78,6 @@ nmds_plot <- ggplot(nmds_data, aes(x = NMDS1, y = NMDS2)) +
 nmds_plot
 
 # un-comment last 3 lines to save plot as svg
-# TODO: change file name
 # svg("CTX_NMDS_Species_Percent.svg")
 # nmds_plot
 # dev.off()
