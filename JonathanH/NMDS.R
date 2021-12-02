8178820d5cfb15ecf2542aa0b70da6b272754b75
Jonathan <82249401+jonathho@users.noreply.github.com>
2021-12-02 15:46:41 -0800

    testing convex hulls

--- JonathanH/NMDS.R
+++ JonathanH/NMDS.R
@@ -20,32 +20,39 @@ library(vegan)
 library(ggplot2)
 
 # TODO: change file path
-datapath <- '2020_thi_kraken2/thi_kraken_all_percent_uncollapsed.tsv'
-
+datapath <- '../2020_clo_kraken2/clo_kraken_all_rawread_uncollapsed.tsv'
 # TODO: setup treatment info
 treat_names <- c("Control", "Acute", "Sublethal")
-
 # TODO: setup replicate info
 rep_names <- c("Rep 1", "Rep 2", "Rep 3", "Rep 5", "Rep 6")
 
-# TODO: give a title for the plot
-plot_title <- "THI NMDS"
 
 # read data
 data <- read.delim(file = datapath,
                        header = TRUE,
                        sep = '\t')
 
-# clean data
+# clean data and filter out non-bacteria
 clean_data <- filter(data, taxRank == "G") %>%
   select(-taxRank, -taxID, -Max, -lineage) %>%
   pivot_longer(!name, names_to = "sample", values_to = "percent") %>%
   pivot_wider(names_from = "name", values_from = "percent")%>%
-  select(-"Apis")
+  select(-"Apis",
+         -"Aspergillus",
+         -"Ascosphaera", 
+         -"Nosema", 
+         -"Crithidia", 
+         -"Lotmaria")
+
+# scale data and convert to data frame
+scaled_data <- apply(clean_data[,2:ncol(clean_data)],
+                     MARGIN = 1,
+                     FUN = function(x) x / sum(x) * 100) %>%
+  t() %>%
+  as.data.frame()
 
 # convert taxa part of data frame to matrix for nmds
-taxa_data <- clean_data[,2:ncol(clean_data)]
-taxa_mat <- as.matrix(taxa_data)
+taxa_mat <- as.matrix(scaled_data)
 
 # perform nmds
 set.seed(1)
@@ -72,18 +79,36 @@ nmds_data$treatment <- factor(nmds_data$treatment,
                               levels = treat_names)
 
 # plot data
+
+# nmds_plot <- ggplot(nmds_data, aes(x = NMDS1, y = NMDS2)) + 
+#   geom_point(aes(colour = replicate, shape = treatment), size = 5) +
+#   labs(title = "NMDS - Bray Curtis",
+#        x = "NMDS1", 
+#        y = "NMDS2",
+#        colour = "Replicate",
+#        shape = "Treatment")
+
+# calculate convex hull
+hull <- nmds_data %>%
+  group_by(treatment) %>%
+  slice(chull(NMDS1, NMDS2))
+
+# make nmds plot
 nmds_plot <- ggplot(nmds_data, aes(x = NMDS1, y = NMDS2)) + 
-  geom_point(aes(shape = treatment, colour = replicate), size = 5) +
-  labs(title = plot_title,
+  geom_point(shape = 21, size = 3) +
+  aes(fill = treatment) +
+  geom_polygon(data = hull, alpha = 0.5) +
+  labs(title = "NMDS - Bray Curtis",
        x = "NMDS1", 
-       y = "NMDS2", 
-       shape = "Treatment", 
-       colour = "Replicate")
+       y = "NMDS2")
 
 nmds_plot
 
+
+
+
 # un-comment last 3 lines to save plot as svg
-# svg("CTX_NMDS_Species_Percent.svg")
+# svg("NMDS_Percent.svg")
 # nmds_plot
 # dev.off()
 
