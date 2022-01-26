312229a41e8f3ceb73261ade931d4e1a89cafe5e
LLansing <35748909+LLansing@users.noreply.github.com>
2022-01-26 10:21:50 -0800

    Create snippet for taxa of interest threshold exploration

---
+++ scripts/snippets/taxa_cutoff_explore.R
@@ -0,0 +1,36 @@
+# This code snippet may be used with the 'scaled clade' table produced in main.R
+# to produce a list of taxa above and below a certain abundance threshold.
+
+# The min_abundance is the percent at which a taxon's relative abundance 
+# must be greater or equal to in at least n samples, where n is the value 
+# specified as min_samples_above_cutoff
+
+# Minimum percent abundance for a taxon
+min_abundance <- 1
+# Minimum number of samples that a taxon must exceed the abundance cutoff
+min_samples_above_cutoff <- 2
+
+
+pc_sc <- select(sc, matches('.*(Reads|name|Rank).*'))
+bac_sc <- pc_sc %>% filter(name == 'Bacteria')
+
+# Get proportions using Bacteria count as total
+pc_sc[c(-1,-2)] <- sapply(X=colnames(pc_sc[c(-1,-2)]),
+                      FUN = function(colname){
+                        col <- pc_sc[,colname]
+                        #print(bac_sc[[colname]])
+                        mutate(col / bac_sc[[colname]] * 100)
+                      })
+
+# Get table of taxa above cutoff
+taxa_above_cutoff <- pc_sc %>% 
+  mutate(num_samples_above_thresh = rowSums(pc_sc[c(-1,-2)] >= min_abundance)) %>% 
+  filter(num_samples_above_thresh >= min_samples_above_cutoff)
+
+# Get table of taxa below cutoff
+taxa_below_cutoff <- pc_sc %>% 
+  mutate(num_samples_above_thresh = rowSums(pc_sc[c(-1,-2)] >= min_abundance)) %>% 
+  filter(num_samples_above_thresh < min_samples_above_cutoff)
+
+write_csv(taxa_above_cutoff[c(1,2)], file=str_glue("taxa_above_cutoff_{min_abundance}p.csv"))
+write_csv(taxa_below_cutoff[c(1,2)], file=str_glue("taxa_below_cutoff_{min_abundance}p.csv"))
