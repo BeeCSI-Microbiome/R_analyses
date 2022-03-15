4e4fa7b10d4f29c6ba889de284cd0240a43970b9
Jonathan <ho.ys.jonathan@gmail.com>
2022-03-15 13:35:41 -0700

    Start ancombc

---
+++ JonathanH/test_ANCOMBC.R
@@ -0,0 +1,6 @@
+# This script is for testing ANCOM_BC as another method for differential
+# abundance. This is to be done before adding to the main R workflow.
+
+# Author(s): Jonathan Ho
+# Updated: Mar 15, 2022
+
