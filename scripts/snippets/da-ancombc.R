1a8f5e8d75c4822335be689b2ace61ea186cf494
Jonathan <ho.ys.jonathan@gmail.com>
2022-04-11 17:34:05 -0700

    generalized ancombc setup

--- scripts/snippets/da-ancombc.R
+++ scripts/snippets/da-ancombc.R
@@ -1,9 +1,10 @@
 # Script for running ANCOM-BC for for pairwise differential 
 # abundance analysis and producing volcano plots.
-# Assumptions: using treatment+replicate for formula
 # Takes two functions from exploratory_functions.R for data wrangling:
 # tidy_data and treat_reps
-# Takes as input: raw clade counts from entire data set
+# Takes as input: raw clade counts (should not be scaled/normalized)
+# Uses treatment+replicate for as the model, but this can be 
+# customized in the ancombc function itself.
 
 # Author(s): Jonathan Ho
 # Updated: Apr 11, 2022
@@ -18,7 +19,7 @@ treat_names <- c("Control","Acute", "Sublethal")
 rep_names <- c("Rep 2", "Rep 3", "Rep 4", "Rep 5", "Rep 6")
 plot_title <- "Caged Control vs Acute THI - Genus DA"
 plot_file_name <- 'thi_control_acute_genus_treatrep.png'
-summary_file_name <- "thi_2020_genus_mod2_trunc.csv"
+summary_file_name <- "thi_2020_genus_mod_trunc.csv"
 
 
 # Functions from exploratory_functions.R ----------------------------------
@@ -60,24 +61,103 @@ treat_reps <- function(d, treat_names, rep_names) {
 }
 
 
-# Setup -------------------------------------------------------------------
-# do both genus and species level
-data <- read_csv(datapath) %>%
-  filter(taxRank == "G")
+# ANCOM-BC ----------------------------------------------------------------
+prep_data <- function(datapath, treat_names, rep_names, rank) {
+  data <- read_csv(datapath) %>%
+    filter(taxRank == rank)
+  
+  # setup abundance data
+  abun_data <- select(data, -taxRank, -lineage) %>%
+    column_to_rownames('name') %>%
+    as.matrix()
+  
+  # setup metadata
+  metadata <- tidy_data(data) %>%
+    select(sample) %>%
+    treat_reps(treat_names, rep_names) %>%
+    column_to_rownames('sample')
+  
+  # combine into phyloseq object
+  OTU <- otu_table(abun_data, taxa_are_rows = T)
+  samples <- sample_data(metadata)
+  
+  phylo_obj <- phyloseq(OTU, samples)
+  
+  return(phylo_obj)
+  
+}
 
-abun_data <- select(data, -taxRank, -lineage) %>%
-  column_to_rownames('name') %>%
-  as.matrix()
+genus_obj <- prep_data(datapath, treat_names, rep_names, "G")
 
-metadata <- tidy_data(data) %>%
-  select(sample) %>%
-  treat_reps(treat_names, rep_names) %>%
-  column_to_rownames('sample') %>%
-  add_labs(lab_names, treat_names)
+# run ancombc
+mod <- ancombc(genus_obj,
+               formula = 'treatment+replicate',
+               p_adj_method = "BH")
 
-OTU <- otu_table(abun_data, taxa_are_rows = T)
-samples <- sample_data(metadata)
+# optionally inspect result using
+# mod$res
 
-phylo_obj <- phyloseq(OTU, samples)
+# Visualize ---------------------------------------------------------------
 
+# only get treatment related betas, adj p-vals, and diffs
+df <- data.frame(mod$res) %>%
+  select(contains('treatment')) %>%
+  select(contains('beta')|contains('q_val')|contains('diff'))
+
+# should work regardless of number of treatments and for every treatment
+# paste('treatment', treat_names[2], sep = '')
+
+df <- mutate(df, acute_diff_abun = ifelse(df$q_val.treatmentAcute < 0.05,
+                                          ifelse(df$beta.treatmentAcute >= 0,
+                                                 'Increase',
+                                                 'Decrease'),
+                                          'No Change'))
+
+df <- mutate(df, sublethal_diff_abun = ifelse(df$q_val.treatmentSublethal < 0.05,
+                                              ifelse(df$beta.treatmentSublethal >= 0,
+                                                     'Increase',
+                                                     'Decrease'),
+                                              'No Change'))
+
+# adjust factor levels for plotting
+df$acute_diff_abun <- factor(df$acute_diff_abun,
+                             levels = c('Increase', 'No Change', 'Decrease'))
+df$sublethal_diff_abun <- factor(df$sublethal_diff_abun,
+                                 levels = c('Increase', 'No Change', 'Decrease'))
+
+da_plot <- ggplot(df,
+                  aes(x = beta.treatmentAcute,
+                      y = -log10(q_val.treatmentAcute),
+                      colour = acute_diff_abun)) +
+  geom_point(alpha=0.5, size=3) +
+  # may need to modify colour values based on whether there are any increases/decreases
+  scale_color_manual(values=c('blue', 'black', 'red')) +
+  # should check that limits allow all points to be visible
+  xlim(c(-2,2)) +
+  geom_vline(xintercept=c(-1,1),lty=4,col="black",lwd=0.8) +
+  geom_hline(yintercept = -log10(0.05), lty=4,col="black",lwd=0.8) +
+  labs(x = "ln(fold change)",
+       y = "-log10(adj. p-value)",
+       title = plot_title) +
+  theme(legend.position = "right",
+        legend.title = element_blank())
+
+da_plot
+
+
+# Save Results ------------------------------------------------------------
+ggsave(da_plot,
+       filename = plot_file_name)
+
+# sort and reorder data frame
+# df <- rename(df,
+#              adj_p_val = q_val.treatmentTHI ,
+#              ln_fold_change = beta.treatmentTHI) %>%
+#   select(-diff_abn.treatmentTHI)
+# 
+# df <- df[with(df, order(adj_p_val, -ln_fold_change)),]
+
+write.csv(df,
+          file = summary_file_name,
+          row.names = T)
 
