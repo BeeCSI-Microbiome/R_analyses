9f735fac9464a692b1862ed3877021d21600954b
Jonathan <ho.ys.jonathan@gmail.com>
2022-04-12 20:04:16 -0700

    small bugfix and remove testing code

--- scripts/snippets/da-ancombc.R
+++ scripts/snippets/da-ancombc.R
@@ -14,11 +14,11 @@ library(ANCOMBC)
 library(microbiome)
 
 # User Defined Variables --------------------------------------------------
-datapath <- 'results/thi_2020/plot_data/thi_raw_clade.csv'
+datapath <- 'results/clo_2020/plot_data/clo_raw_clade.csv'
 treat_names <- c("Control","Acute","Sublethal")
-rep_names <- c("Rep 2", "Rep 3", "Rep 4", "Rep 5", "Rep 6")
+rep_names <- c("Rep 1", "Rep 2", "Rep 3", "Rep 5", "Rep 6")
 base_title <- 'Caged Control vs'
-dataset_name <- 'thi_2020'
+dataset_name <- 'clo_2020'
 # plot_file_name <- 'thi_control_acute_genus_treatrep.png'
 summary_file_name <- "thi_2020_genus_mod_trunc.csv"
 
@@ -88,19 +88,14 @@ prep_data <- function(datapath, treat_names, rep_names, rank) {
   
 }
 
-genus_obj <- prep_data(datapath, treat_names, rep_names, "G")
 
-# run ancombc
-mod <- ancombc(genus_obj,
-               formula = 'treatment+replicate',
-               p_adj_method = "BH")
 
 # optionally inspect result using
 # mod$res
 
 # Visualize ---------------------------------------------------------------
 # setup data frame for plotting and later saving
-prep_plot <- function(ancombc_mod, treat_names) {
+visualize_save <- function(ancombc_mod, treat_names) {
   
   # only get treatment related betas, adj p-vals, and diffs
   df <- data.frame(mod$res) %>%
@@ -118,6 +113,7 @@ prep_plot <- function(ancombc_mod, treat_names) {
       sym()
     beta_i <- paste('beta.treatment', i, sep = '') %>%
       sym()
+    plot_title_i <- paste(base_title, i, '-', dataset_name)
     
     # determines whether DA are increases or decreases
     df <- mutate(df, !!diff_abun_i := ifelse(df[,grep(q_val_i, 
@@ -132,11 +128,16 @@ prep_plot <- function(ancombc_mod, treat_names) {
     df[,grep(diff_abun_i, colnames(df))] <- factor(df[,grep(diff_abun_i, colnames(df))],
                                                    levels = c('Increase', 'No Change', 'Decrease'))
     
+    # plot and save
+    da_plot <- volc_plot(df, beta_i, q_val_i, diff_abun_i, plot_title_i)
+    
+    ggsave(da_plot,
+           filename = paste(plot_title_i, '.png', sep = ''))
   }
 
-  return(df)
+  # return(df)
 }
-
+          
 # plots volcano plot for given data and column names in data frame
 volc_plot <- function(df, beta, qval, da, plot_title) {
 
@@ -148,8 +149,9 @@ volc_plot <- function(df, beta, qval, da, plot_title) {
     scale_color_manual(values=c('Increase' = 'blue',
                                 'No Change' = 'black',
                                 'Decrease' = 'red')) +
-    xlim(0-abs(max(df[,grep(beta,colnames(df))])+0.25), 
-         0+abs(max(df[,grep(beta,colnames(df))])+0.25)) +
+    # TODO: use ifelse to set default so lines always visible?
+    xlim(0-max(abs(df[,grep(beta,colnames(df))])+0.25), 
+         0+max(abs(df[,grep(beta,colnames(df))])+0.25)) +
     geom_vline(xintercept=c(-1,1),lty=4,col="black",lwd=0.8) +
     geom_hline(yintercept = -log10(0.05), lty=4,col="black",lwd=0.8) +
     labs(x = "ln(fold change)",
@@ -161,160 +163,19 @@ volc_plot <- function(df, beta, qval, da, plot_title) {
   return(da_plot)
 }
 
+# TODO: use single function to combine these 3 and do both genus and species
+genus_obj <- prep_data(datapath, treat_names, rep_names, "G")
 
-# generates and saves volcano plot for every pairwise treatment
-plot_and_save <- function(df, treat_names, base_title, dataset_name) {
-  # define baseline for comparison(s)
-  controls <- c('Control', 'unexposed')
-  just_treats <- treat_names[!treat_names %in% controls]
-  for (i in just_treats) {
-    # setup identifiable strings for i-th treatment
-    diff_abun_i <- paste('diff_abun_', i, sep = '') %>%
-      sym()
-    q_val_i <- paste('q_val.treatment', i, sep = '') %>%
-      sym()
-    beta_i <- paste('beta.treatment', i, sep = '') %>%
-      sym()
-    plot_title_i <- paste(base_title, i, '-', dataset_name)
-    
-    # call prep here instead?
-    
-    da_plot <- volc_plot(df, beta_i, q_val_i, diff_abun_i, plot_title_i)
-    
-    ggsave(da_plot,
-           filename = paste(plot_title_i, '.png', sep = ''))
-  }
-  
-}
-# # only get treatment related betas, adj p-vals, and diffs
-# df <- data.frame(mod$res) %>%
-#   select(contains('treatment')) %>%
-#   select(contains('beta')|contains('q_val')|contains('diff'))
-# 
-# controls <- c('Control', 'unexposed')
-# just_treats <- treat_names[!treat_names %in% controls]
-# for (i in just_treats) {
-#   # setup identifiable strings for i-th treatment
-#   diff_abun_i <- paste('diff_abun_', i, sep = '') %>%
-#     sym()
-#   q_val_i <- paste('q_val.treatment', i, sep = '') %>%
-#     sym()
-#   beta_i <- paste('beta.treatment', i, sep = '') %>%
-#     sym()
-#   plot_title_i <- paste(base_title, i, '-', dataset_name)
-#   
-#   # determines whether DA are increases or decreases
-#   df <- mutate(df, !!diff_abun_i := ifelse(df[,grep(q_val_i, 
-#                                                     colnames(df))] < 0.05,
-#                                            ifelse(df[,grep(beta_i,
-#                                                            colnames(df))] >= 0,
-#                                                   'Increase',
-#                                                   'Decrease'),
-#                                            'No Change'))
-#   
-#   # adjust factor levels for plotting
-#   df[,grep(diff_abun_i, colnames(df))] <- factor(df[,grep(diff_abun_i, colnames(df))],
-#                                                  levels = c('Increase', 'No Change', 'Decrease'))
-# 
-#   da_plot <- ggplot(df,
-#          aes(x = !!beta_i,
-#              y = -log10(!!q_val_i),
-#              colour = !!diff_abun_i)) +
-#     geom_point(alpha=0.5, size=3) +
-#     scale_color_manual(values=c('Increase' = 'blue',
-#                                 'No Change' = 'black',
-#                                 'Decrease' = 'red')) +
-#     xlim(0-abs(max(df[,grep(beta_i,colnames(df))])+0.25), 
-#          0+abs(max(df[,grep(beta_i,colnames(df))])+0.25)) +
-#     geom_vline(xintercept=c(-1,1),lty=4,col="black",lwd=0.8) +
-#     geom_hline(yintercept = -log10(0.05), lty=4,col="black",lwd=0.8) +
-#     labs(x = "ln(fold change)",
-#          y = "-log10(adj. p-value)",
-#          title = plot_title_i) +
-#     theme(legend.position = "right",
-#           legend.title = element_blank())
-#   
-#   ggsave(da_plot,
-#          filename = paste(plot_title_i, '.png', sep = ''))
-# }
+# run ancombc
+mod <- ancombc(genus_obj,
+               formula = 'treatment+replicate',
+               p_adj_method = "BH")
+visualize_save(mod, treat_names)
 
-
-
-# diff_abun_i <- paste('diff_abun_', treat_names[2], sep = '') %>%
-#   sym()
-# q_val_i <- paste('q_val.treatment', treat_names[2], sep = '') %>%
-#   sym()
-# beta_i <- paste('beta.treatment', treat_names[2], sep = '') %>%
-#   sym()
-# plot_title_i <- paste(base_title, treat_names[2], '-', dataset_name)
-
-
-df_test <- prep_plot(mod, treat_names)
-plot_and_save(df_test, treat_names, base_title, dataset_name)
-
-# ggplot(df,
-#        aes(x = !!beta_i,
-#            y = -log10(!!q_val_i),
-#            colour = !!diff_abun_i)) +
-#   geom_point(alpha=0.5, size=3) +
-#   scale_color_manual(values=c('Increase' = 'blue',
-#                               'No Change' = 'black',
-#                               'Decrease' = 'red')) +
-#   xlim(0-abs(max(df[,grep(beta_i,colnames(df))])+0.25), 
-#        0+abs(max(df[,grep(beta_i,colnames(df))])+0.25)) +
-#   geom_vline(xintercept=c(-1,1),lty=4,col="black",lwd=0.8) +
-#   geom_hline(yintercept = -log10(0.05), lty=4,col="black",lwd=0.8) +
-#   labs(x = "ln(fold change)",
-#        y = "-log10(adj. p-value)",
-#        title = test_title) +
-#   theme(legend.position = "right",
-#         legend.title = element_blank())
-
-
-############################## Testing above
-
-# df <- mutate(df, acute_diff_abun = ifelse(df$q_val.treatmentAcute < 0.05,
-#                                           ifelse(df$beta.treatmentAcute >= 0,
-#                                                  'Increase',
-#                                                  'Decrease'),
-#                                           'No Change'))
-# 
-# df <- mutate(df, sublethal_diff_abun = ifelse(df$q_val.treatmentSublethal < 0.05,
-#                                               ifelse(df$beta.treatmentSublethal >= 0,
-#                                                      'Increase',
-#                                                      'Decrease'),
-#                                               'No Change'))
-# 
-# # adjust factor levels for plotting
-# df$acute_diff_abun <- factor(df$acute_diff_abun,
-#                              levels = c('Increase', 'No Change', 'Decrease'))
-# df$sublethal_diff_abun <- factor(df$sublethal_diff_abun,
-#                                  levels = c('Increase', 'No Change', 'Decrease'))
-
-# da_plot <- ggplot(df,
-#                   aes(x = beta.treatmentAcute,
-#                       y = -log10(q_val.treatmentAcute),
-#                       colour = diff_abun_Acute)) +
-#   geom_point(alpha=0.5, size=3) +
-#   scale_color_manual(values=c('Increase' = 'blue',
-#                               'No Change' = 'black',
-#                               'Decrease' = 'red')) +
-#   xlim(0-abs(max(df$beta.treatmentAcute)+0.25), 
-#        0+abs(max(df$beta.treatmentAcute)+0.25)) +
-#   geom_vline(xintercept=c(-1,1),lty=4,col="black",lwd=0.8) +
-#   geom_hline(yintercept = -log10(0.05), lty=4,col="black",lwd=0.8) +
-#   labs(x = "ln(fold change)",
-#        y = "-log10(adj. p-value)",
-#        title = plot_title) +
-#   theme(legend.position = "right",
-#         legend.title = element_blank())
-# 
-# da_plot
 
 
 # Save Results ------------------------------------------------------------
-# ggsave(da_plot,
-#        filename = plot_file_name)
+# TODO: incorporate saving df and sorting by significant q values
 
 # sort and reorder data frame
 # df <- rename(df,
