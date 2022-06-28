a335b27cadc0ff36e5db65620ca58aad9e9b0e96
emmaxjean <77707364+emmaxjean@users.noreply.github.com>
2022-06-28 09:50:20 -0500

    Update indicator_taxa_analysis.R
    Simplified regex expression part

--- scripts/snippets/indicator_taxa_analysis.R
+++ scripts/snippets/indicator_taxa_analysis.R
@@ -21,80 +21,32 @@ library(glue)
 
 
 
-
-####################################################
-## takes in the tt table from run_indicator_analysis
-## returns the type of sample id present 
-####################################################
-id_type <- function(tt){
-  
-  # 1) ABC{*}{e,u}_t{*}
-  # 2) ABC{*}_d{*}
-  # 3) A_CTL{*}_d0 and A_BCD{*}_d{*}
-  # 4) ABC{*}_d{*}_t{*}
-  # 5) ABC{*}{e,u}
-  
-  nt <- tt %>%
-    select(-c(name, taxRank, taxID, depth, taxLineage))
-  tester <- colnames(nt)[1]
-  parts <- unlist(str_split(tester, ""), use.names=FALSE)
-  counts <- as.data.frame(table(parts[1:3]))
-  if("_" %in% counts$Var1){
-    which1 <- "3"
-  } else{
-    counts2 <- as.data.frame(table(parts))
-    how_many2 <- filter(counts2, parts == "_")[,2]
-    if(identical(how_many2, integer(0))){
-      which1 <- "5"
-    } else if(how_many2 == 2){
-      which1 <- "4"
-    } else{
-      num_letters <- str_count(tester, "\\D") - how_many
-      if(num_letters == 5){
-        which1 <- "1"
-      }
-      if(num_letters == 4){
-        which1 <- "2"
-      }
-    }
-  }
-  return(which1)
-}
-
 #####################################################
-## takes in the tt table from run_indicator_analysis,
-##    the dataset name, and the type of sample id
-##    present
+## takes in activity type
 ## returns the regular expression to use in pivoting
 #####################################################
-reg_ex <- function(d_name, id_num, tt){
-  if(id_num == 1){
-    reg <- paste0("(\\D\\D\\D(..)([e|u])_t(.)).*")
+reg_ex <- function(activity){
+  if(activity == 1){
+    reg <- paste0("((?:(?:\\w_){0,1}\\w{3})(\\d\\d)_d(\\d)(?:_t(\\d)){0,1}).*")
   } 
-  if(id_num == 2){
-    reg <- paste0("(\\D\\D\\D(..)_d(.)).*")
-  }
-  if(id_num == 3){
-    reg <- "((?:(?:\\w_){0,1}\\w{3})(\\d\\d)_d(\\d)).*"
-  } 
-  if(id_num == 4){
-    reg <- paste0("(\\D\\D\\D(..)_d(.)_t(.)).*")
-  }
-  if(id_num == 5){
-    reg <- paste0("(\\D\\D\\D(..)([e|u])).*")
+  if(activity == 2){
+    reg <- paste0("(\\D\\D\\D(..)([e|u])(?:_t(\\d)){0,1}).*")
   }
   return(reg)
 }
 
 #####################################################
-## takes in the the type of sample id present
+## takes in the regular expression being used and one
+##    sample id from a column in tt
 ## returns the metadata columns to use when pivoting
 #####################################################
-col_names <- function(id_num){
-  if(id_num == 1 | id_num == 4){
+col_names <- function(reg_ex, sampleID){
+  matches <- str_match(sampleID, reg_ex)
+  matches <- matches[!is.na(matches)]
+  if(length(matches) == 5){
     cols <- c("sample", "replicate", "treatment", "time")
   }
-  if(id_num == 2 | id_num == 3 | id_num ==5){
+  if(length(matches) == 4){
     cols <- c("sample", "replicate", "treatment")
   }
   return(cols)
@@ -102,13 +54,12 @@ col_names <- function(id_num){
 
 #####################################################
 ## takes in the tt table from run indicator analysis
-##    and the sample id type present
+##    and the metadata columns being used
 ## returns the proportions for each taxa as a table
 #####################################################
-calc_prop <- function(tt, type) {
-  meta_cols <- col_names(type)
+calc_prop <- function(tt, meta_cols) {
   sample_col <- select(tt, all_of(meta_cols))
-  prop_data <- as.matrix(select(tt, -meta_cols)) %>%
+  prop_data <- as.matrix(select(tt, -all_of(meta_cols))) %>%
     apply(MARGIN = 1, FUN = function(x) x / sum(x) * 100) %>%
     t() %>%
     as.data.frame() %>%
@@ -119,22 +70,23 @@ calc_prop <- function(tt, type) {
 }
 
 #####################################################
-## takes in dataset name (same as in main.R), the 
-##    string for the desired table (e.g. "raw_taxon"),
-##    the analysis function you'd like to use, and
-##    the rank_symbol you'd like
-## Does the indicator taxa anlysis and writes those 
+## takes in data set name (same as in main.R), the 
+##    activity type of the data set, and the string 
+##    for the desired table (e.g. "raw_taxon"), the 
+##    analysis function you'd like to use, and the
+##    rank_symbol you'd like
+## Does the indicator taxa analysis and writes those 
 ##    results along with adjusted p-values to 2 files
 ##    (one for treatment and one for replicate)
 #####################################################
-run_indicator_analysis <- function(dataset_name, table_string, analysis_func, rank_symbol="all") {
+run_indicator_analysis <- function(dataset_name, activity, table_string, analysis_func, rank_symbol="all") {
   # Create output dir if it doesn't exist yet
   ind_sp_dir <- glue("results/{dataset_name}/indicator_species_analysis")
   ifelse(!dir.exists(ind_sp_dir),
          dir.create(ind_sp_dir, mode = "777"), FALSE)
   
   # read in tables files from the relevant results folder
-  file <- paste0("results/",dataset_name,"/",table_string,"_counts.csv")
+  file <- paste0("results/", dataset_name,"/", table_string,"_counts.csv")
   table <- read.csv(file)
   if(rank_symbol == "all"){
     tt <- table
@@ -145,18 +97,15 @@ run_indicator_analysis <- function(dataset_name, table_string, analysis_func, ra
   # Pivot tables into wide format (1 column per taxa, 1 row per sample) and
   # create metadata category columns
   d_nm <- substr(dataset_name, 1, nchar(dataset_name)-5)
-  type <- id_type(tt)
-  if(type==3){
-    d_ctl <- paste0(substring(d_nm, 1, 2), "ctl")
-  } else{
-    d_ctl <- d_nm
-  }
-  cols <- col_names(type)
+  d_ctl <- paste0(substring(d_nm, 1, 2), "ctl")
+  regx <- reg_ex(activity)
+  sample_id <- colnames((tt %>% select(-c("name", "taxRank", "taxID", "depth", "taxLineage")))[1])
+  meta_cols <- col_names(regx, sample_id)
   tt <- tt %>%
     pivot_longer(
       cols = contains(c(d_nm, d_ctl)), 
-      names_to = cols,
-      names_pattern = reg_ex(dataset_name, type, tt), 
+      names_to = meta_cols,
+      names_pattern = regx, 
       values_to = "read_count"
     )
   
@@ -165,7 +114,7 @@ run_indicator_analysis <- function(dataset_name, table_string, analysis_func, ra
   tt$treatment[which(tt$treatment=="e")] <- "Treatment"
   tt$treatment[which(tt$treatment=="0" | tt$treatment=="u")] <- "Control"
   tt$replicate <- paste0("Rep ", tt$replicate)
-
+  
   # Add taxa IDs to the names in order to deal with duplicate names 
   name_and_id <- paste0(tt$name, " (", tt$taxID, ")")
   tt <- cbind(name_and_id, tt)
@@ -175,10 +124,10 @@ run_indicator_analysis <- function(dataset_name, table_string, analysis_func, ra
     pivot_wider(names_from = "name_and_id", values_from = "read_count") 
   
   # calculate proportions
-  tt <- calc_prop(tt, type)
+  tt <- calc_prop(tt, meta_cols)
   
   # Extract count table and metadata vectors
-  tt_abund <- tt[,(length(cols)+1):ncol(tt)] 
+  tt_abund <- tt[,(length(meta_cols)+1):ncol(tt)] 
   tt_treat <- tt$treatment
   tt_rep <- tt$replicate
   
@@ -223,16 +172,18 @@ run_indicator_analysis <- function(dataset_name, table_string, analysis_func, ra
 
 
 
+
 # dataset to run analysis on
 dataset_name <- "pdv_2021"
+activity <- 1
 
 # run for all taxa
-run_indicator_analysis(dataset_name, "raw_taxon", "r.g")
-run_indicator_analysis(dataset_name, "raw_clade", "r.g")
+run_indicator_analysis(dataset_name, activity, "raw_taxon", "r.g")
+run_indicator_analysis(dataset_name, activity, "raw_clade", "r.g")
 # run for species taxa
-run_indicator_analysis(dataset_name, "raw_taxon", "r.g", "S")
-run_indicator_analysis(dataset_name, "raw_clade", "r.g", "S")
+run_indicator_analysis(dataset_name, activity, "raw_taxon", "r.g", "S")
+run_indicator_analysis(dataset_name, activity, "raw_clade", "r.g", "S")
 # run for genus taxa
-run_indicator_analysis(dataset_name, "raw_taxon", "r.g", "G")
-run_indicator_analysis(dataset_name, "raw_clade", "r.g", "G")
+run_indicator_analysis(dataset_name, activity, "raw_taxon", "r.g", "G")
+run_indicator_analysis(dataset_name, activity, "raw_clade", "r.g", "G")
 
