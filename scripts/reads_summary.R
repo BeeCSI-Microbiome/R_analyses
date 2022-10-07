9fcca0a10788f1376ff63da448b7aa467fe248f8
LLansing <35748909+LLansing@users.noreply.github.com>
2022-10-07 13:16:00 -0700

    Create "Other Eukaryota" grouping

--- scripts/reads_summary.R
+++ scripts/reads_summary.R
@@ -13,8 +13,9 @@ taxa_list <- c("root", # total classified reads in a sample
                "unclassified",
                "Apis mellifera", # host
                "Bacteria",
-               "Metazoa", 
-               "Fungi")
+               "Fungi",
+               "Other Eukaryota"
+  )
 # ------------------------------------------------------------------------------
 
 
@@ -22,33 +23,46 @@ taxa_list <- c("root", # total classified reads in a sample
 
 export("create_summary_table")
 create_summary_table <- function(read_table, dataset_name){
-  
+  # Rename a duplicate taxa name
+  read_table <- read_table %>% mutate(name = case_when(
+    name == "Actinobacteria" & taxRank == "C" ~ "Actinomycetia",
+    TRUE ~ name
+  ))
+
+  # Filter columns
+  read_table <- read_table %>% select(name, contains("cladeReads"))
+
+  # Calculate row for Other Eukaryota
+  read_table <- read_table %>% add_row(name="Other Eukaryota") %>%
+    column_to_rownames("name")
+  read_table["Other Eukaryota",] <- read_table["Eukaryota",] - read_table["Fungi",] - read_table["Apis mellifera",]
+
   # Pivot samples to a column, keep only taxa in taxa_list
-  ta <- subset(read_table, name %in% taxa_list) %>% 
-    select_if(str_detect(names(.), "name|cladeReads")) %>% 
+  ta <- read_table %>% rownames_to_column(var = "name") %>%
+    subset(name %in% taxa_list) %>%
     tidyr::pivot_longer(
       cols = contains("cladeReads"),
       names_to = "sample",
       values_to = "reads"
     )
-  
+
   # sample name formatting
   ta$sample <- gsub("(.*)\\.cladeReads", "\\1", ta$sample)
-  
+
   # Pivot to get taxa as columns
-  ta <- ta %>% 
+  ta <- ta %>%
     tidyr::pivot_wider(
       names_from = "name",
       values_from = c("reads")
     )
-  
+
   # some column name formatting
   ta <- ta %>% dplyr::rename(classified = root) %>%
     dplyr::rename_with(~ gsub(" ", "_", .x)) %>%
     dplyr::rename_with(~ str_c(.x, "_reads"), !matches("sample"))
-  
+
   # create total read count column
-  ta <- ta %>% 
+  ta <- ta %>%
     dplyr::mutate(ta, total_reads = unclassified_reads+classified_reads,
                   .after = sample) %>%
     # percent classified
@@ -57,18 +71,18 @@ create_summary_table <- function(read_table, dataset_name){
     # percent unclassified
     dplyr::mutate(percent_unclassified = unclassified_reads/total_reads * 100,
                   .after = unclassified_reads)
-  
+
   # create "percent of classified reads" columns for taxa
   ta <- ta %>% dplyr::mutate(across(
     !matches("sample|total_reads|*classified*"),
     ~ .x/classified_reads*100,
     .names = "percent_classified_{gsub('_reads', '', .col)}"
   ))
-  
+
   # Drop read # columns for taxa
-  ta <- ta %>% select(!matches("(?<!total|classified|Bacteria|Apis_mellifera|Metazoa|Fungi)_reads", perl = TRUE))
+  ta <- ta %>% select(!matches("(?<!total)(classified|Bacteria|Apis_mellifera|Other_Eukaryota|Fungi)_reads", perl = TRUE))
   ta <- ta %>% relocate("percent_classified_Bacteria", .after = "percent_classified_Apis_mellifera")
-  
+
   write.table(ta, file = glue("results/{dataset_name}/{dataset_name}_read_summary.csv"), quote = F, sep = ",", row.names = F)
   return(ta)
 }
